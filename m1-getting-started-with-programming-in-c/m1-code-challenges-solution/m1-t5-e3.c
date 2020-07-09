4c1c49feb0e4399af66f9e75ccb764b0ac96000a
SUPRIYODEY886 <66872286+SUPRIYODEY886@users.noreply.github.com>
2020-07-09 22:17:29 +0530

    Update m1-t5-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -2,8 +2,8 @@
 
     int main() {
 
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
+        printf("I am Supriyo Dey.");
+        puts("I am 21 years old.");
 
         return 0;
     }
